051f73363386b2fa3afd56be073e337ee5c8bcb6
liuhao <liuhaoguoguo.1002@gmail.com>
2021-02-19 16:18:57 +0800

    提交盆友圈页面

--- app/src/main/java/com/liuhao/wcwork/MainActivity.kt
+++ app/src/main/java/com/liuhao/wcwork/MainActivity.kt
@@ -2,11 +2,17 @@ package com.liuhao.wcwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.liuhao.wcwork.common.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_go_btn).setOnClickListener {
+            startActivity<FriendsCircleActivity>()
+        }
     }
 }
